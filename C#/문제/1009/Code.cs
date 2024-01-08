d564875bce3bd308420dd2c381136415a238abad
BSF0124 <dbsdbs2580@gmail.com>
2024-01-08 23:36:44 +0900

    2024.01.08

---
+++ C#/문제/1009/Code.cs
@@ -0,0 +1,20 @@
+﻿int t = int.Parse(Console.ReadLine());
+
+for(int i=0; i<t; i++)
+{
+    string[] str = Console.ReadLine().Split();
+
+    int a = int.Parse(str[0]) % 10;
+    int b = int.Parse(str[1]);
+    int cpu = 1;
+
+    for(int j=1; j<=b; j++)
+    {
+        cpu *= a;
+        cpu %= 10;
+    }
+    
+    if(cpu == 0) cpu=10;
+    
+    Console.WriteLine(cpu);
+}
---
+++ C#/문제/1085/Code.cs
@@ -0,0 +1,16 @@
+﻿string[] str = Console.ReadLine().Split();
+
+int x = int.Parse(str[0]);
+int y = int.Parse(str[1]);
+int w = int.Parse(str[2]);
+int h = int.Parse(str[3]);
+
+List<int> distance = new List<int>();
+
+distance.Add(x);
+distance.Add(y);
+distance.Add(w-x);
+distance.Add(h-y);
+distance.Sort();
+
+Console.WriteLine(distance[0]);
