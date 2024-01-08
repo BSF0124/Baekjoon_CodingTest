9f02381ad776bb8b95405b74f0f8f3232fbe74ed
BSF0124 <dbsdbs2580@gmail.com>
2024-01-08 23:39:08 +0900

    2024.01.08

---
+++ C#/문제/2292/Code.cs
@@ -0,0 +1,22 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+int i = 1;
+int num = 1;
+int sum = 0;
+
+while(true)
+{
+    sum += num;
+
+    if(n <= sum)
+    {
+        Console.WriteLine(i);
+        break;
+    }
+    
+    else
+    {
+        num = i*6;
+        i++;
+    }
+}
